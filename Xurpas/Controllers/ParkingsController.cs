f4beee7171473ae4b9af1ae2843a50d32aee9f84
josephrey.salundaguit@outlook.com <josephrey.salundaguit@outlook.com>
2020-12-21 11:24:50 +0800

    Add Search and Filter

--- Xurpas/Controllers/ParkingsController.cs
+++ Xurpas/Controllers/ParkingsController.cs
@@ -24,9 +24,50 @@ namespace Xurpas.Controllers
             _services = services;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string sortOrder, string searchString)
         {
             var listparking = _services.GetAllParkings();
+
+            ViewData["ParkingIDSortParm"] = sortOrder == "ParkingId" ? "ParkingId_desc" : "ParkingId";
+            ViewData["DateTimeInSortParm"] = sortOrder == "DateTimeIn" ? "DateTimeIn_desc" : "DateTimeIn";
+            ViewData["DateTimeOutSortParm"] = sortOrder == "DateTimeOut" ? "DateTimeOut_desc" : "DateTimeOut";
+            ViewData["CurrentFilter"] = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                listparking = listparking.Where(s =>
+                                                s.PlateNumber.ToString().ToUpper().Contains(searchString.ToUpper()) ||
+                                                s.ParkingSpaceID.ToString().ToUpper().Contains(searchString.ToUpper()) ||
+                                                s.EntryPointName.ToString().ToUpper().Contains(searchString.ToUpper()) ||
+                                                s.ParkingTypeCode.ToString().ToUpper().Contains(searchString.ToUpper())
+                                                ).ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "ParkingId":
+                    listparking = listparking.OrderBy(x => x.ParkingSpaceID).ToList();
+                    break;
+                case "ParkingId_desc":
+                    listparking = listparking.OrderByDescending(x => x.ParkingSpaceID).ToList();
+                    break;
+                case "DateTimeIn":
+                    listparking = listparking.OrderBy(s => s.TimeIn).ToList();
+                    break;
+                case "DateTimeIn_desc":
+                    listparking = listparking.OrderByDescending(s => s.TimeIn).ToList();
+                    break;
+                case "DateTimeOut":
+                    listparking = listparking.OrderBy(s => s.TimeOut).ToList();
+                    break;
+                case "DateTimeOut_desc":
+                    listparking = listparking.OrderByDescending(s => s.TimeOut).ToList();
+                    break;
+                default:
+                    listparking = listparking.OrderByDescending(s => s.ID).ToList();
+                    break;
+            }
+
             return View(listparking);
         }
 
